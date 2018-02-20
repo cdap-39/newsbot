0260ede9f352c25255b4d097fd1590b88427fbf4
Mahendra Tennakoon <mahendrathennakoon@gmail.com>
2018-02-20 08:38:54 +0530

    Extract content for sub headings

--- newsbot/spiders/newsfirst.py
+++ newsbot/spiders/newsfirst.py
@@ -11,8 +11,22 @@ class NewsfirstSpider(scrapy.Spider):
     def parse(self, response):
 
         # Main headings
-        for news_block in response.xpath("//div[contains(@class, 'main-news-block')]"):
-            heading = news_block.xpath("a/div[contains(@class, 'main-news-heading')]/h1/text()").extract_first()
+        # for news_block in response.xpath("//div[contains(@class, 'main-news-block')]"):
+        #     heading = news_block.xpath("a/div[contains(@class, 'main-news-heading')]/h1/text()").extract_first()
+        #     content_link = news_block.xpath("a/@href").extract_first()
+        #
+        #     # Extract content by following the link.
+        #     item = NewsItem()
+        #
+        #     item['heading'] = heading
+        #     item['link'] = content_link
+        #     request = scrapy.Request(content_link, callback=self.parse_content)
+        #     request.meta['item'] = item
+        #     yield request
+
+        # Sub headings
+        for news_block in response.css('div.sub-1-news-block'):
+            heading = news_block.xpath("a/div[contains(@class, 'sub-1-news-heading')]/h2/text()").extract_first()
             content_link = news_block.xpath("a/@href").extract_first()
 
             # Extract content by following the link.
@@ -23,12 +37,6 @@ class NewsfirstSpider(scrapy.Spider):
             request = scrapy.Request(content_link, callback=self.parse_content)
             request.meta['item'] = item
             yield request
-
-        # Sub headings
-        # for news in response.css('div.sub-1-news-block'):
-        #     headings.append({
-        #         'heading': news.css('div.sub-1-news-heading>h2::text').extract_first()
-        #     })
 
     # Parse content of the news article
     def parse_content(self, response):
