d9c0db5169d318222734a92b3483576a5bf61a79
Mahendra Tennakoon <mahendrathennakoon@gmail.com>
2018-06-22 02:43:29 +0530

    Start URLs for pagination

--- newsbot/spiders/hirunews.py
+++ newsbot/spiders/hirunews.py
@@ -6,7 +6,7 @@ from newsbot.items import NewsItem
 class HirunewsSpider(scrapy.Spider):
     name = 'hirunews'
     allowed_domains = ['hirunews.lk']
-    start_urls = ['http://www.hirunews.lk/local-news.php']
+    start_urls = ['http://www.hirunews.lk/local-news.php?pageID=%d' % page for page in range(1,1842)]
 
     def parse(self, response):
 
